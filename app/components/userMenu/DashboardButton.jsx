4df5cc36c38cb7113f396e144c85ba5ba2e3058f
dominikd <dominikd@cassoviacode.com>
2025-03-31 08:42:46 +0200

    button margin change

--- app/components/userMenu/DashboardButton.jsx
+++ app/components/userMenu/DashboardButton.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const DashboardButton = () => {
   return (
-    <div className="w-full px-6 mt-4 pb-3">
+    <div className="w-full px-3 mt-4 pb-3">
       <button className="w-full bg-brand text-white py-2 rounded-md flex items-center justify-center space-x-2 cursor-pointer">
         <Image src="/icons/profile-white.svg" width={20} height={20} alt="Logo" />
         <span>Nástenka</span>
